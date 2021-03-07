c02cb864ccbf752bffc6294e91a12d418bb37a4d
DTeeam <teamdetecnik@gmail.com>
2021-03-08 00:53:37 +0100

    Fixed sign out, added author name and bug fixes.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,7 @@ module ApplicationHelper
       if user_signed_in?
         concat link_to "Edit account", edit_user_registration_path, method: :get, class: 'btn btn-sm btn-outline-secondary'
         concat " "
-        concat link_to "Sign out", destroy_user_session_path, method: :delete, class: 'btn btn-sm btn-outline-secondary'
+        concat link_to "Sign out", destroy_user_session_path, method: :get, class: 'btn btn-sm btn-outline-secondary'
       else
         concat link_to "Sign in", new_user_session_path, class: 'btn btn-sm btn-outline-secondary'
         concat " "
